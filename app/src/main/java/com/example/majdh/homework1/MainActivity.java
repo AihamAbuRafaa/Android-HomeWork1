b240188557a8a4ef3a7b9a7c60ab98350e77528b
aihamaburafaa <aehm@runnableweb.com>
2019-04-08 23:29:03 +0300

    fixing a bugs

--- app/src/main/java/com/example/majdh/homework1/MainActivity.java
+++ app/src/main/java/com/example/majdh/homework1/MainActivity.java
@@ -41,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     {
         images[0] = R.drawable.ali_connors_img;
         images[1] = R.drawable.aiham_aburafaa_img; //need change
-        images[2] = R.drawable.salam_img; //need change
-        images[3] = R.drawable.ali_connors_img; //need change
+        images[2] = R.drawable.mark_img; //need change
+        images[3] = R.drawable.salam_img; //need change
         images[4] = R.drawable.ali_connors_img; //need change
     }
 }
